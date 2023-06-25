ab74615ec1c518364555a7ad05e2261a6dac2e5c
diomin.vd13 <diomin.vd13@icloud.com>
2023-06-25 13:36:35 +0300

    Base for bot. Structure of files, base config.

---
+++ config.py
@@ -0,0 +1,10 @@
+import aiogram
+import os
+
+
+# API_TOKEN - variable of virtual environment.
+bot: aiogram.Bot = aiogram.Bot(token=os.environ['API_TOKEN'], parse_mode='HTML')
+dispatcher: aiogram.Dispatcher = aiogram.Dispatcher()
+
+# BOT_ROM is necessary to monitor active operations that are in the process of being created.
+BOT_RAM: dict = {}
