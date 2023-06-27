bda7e4a4d33b9355ccd706ad4afd68e678d6f8a5
diomin.vd13 <diomin.vd13@icloud.com>
2023-06-27 22:22:50 +0300

    v1.0
    Add instruction for deploy in config.py.

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 
 
 # API_TOKEN - variable of virtual environment.
+# For deploy bot on server replace 'os.environ['API_TOKEN']' to bot token.
 bot: aiogram.Bot = aiogram.Bot(token=os.environ['API_TOKEN'], parse_mode='HTML')
 dispatcher: aiogram.Dispatcher = aiogram.Dispatcher()
 
