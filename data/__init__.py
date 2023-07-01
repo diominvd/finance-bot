8cb369d7afdfc82238e015e3fc897d698b35a823
diomin.vd13 <diomin.vd13@icloud.com>
2023-07-01 20:33:00 +0300

    Switching from SQLite to MySQL.

--- data/__init__.py
+++ data/__init__.py
@@ -1,33 +1,36 @@
-import sqlite3
+import mysql.connector as connector
+import os
 
 
-connection = sqlite3.connect('data/database.db')
+# change venv variables when uploading to the server .
+connection = connector.connect(host=os.environ['HOST'], user=os.environ['USER'], password=os.environ['PASSWORD'], db=os.environ['DB'])
 cursor = connection.cursor()
 
 # Database -> users.
 cursor.execute('''CREATE TABLE IF NOT EXISTS users(
-    user_id INTEGER PRIMARY KEY UNIQUE,
-    products REAL,
-    cafes REAL,
-    auto REAL,
-    transport REAL,
-    home REAL,
-    entertainment REAL,
-    sport REAL,
-    health REAL,
-    education REAL,
-    gifts REAL,
-    beauty REAL,
-    clothes REAL,
-    technic REAL,
-    subscriptions REAL,
-    total REAL)''')
+    user_id INT PRIMARY KEY UNIQUE,
+    products FLOAT,
+    cafes FLOAT,
+    auto FLOAT,
+    transport FLOAT,
+    home FLOAT,
+    entertainment FLOAT,
+    sport FLOAT,
+    health FLOAT,
+    education FLOAT,
+    gifts FLOAT,
+    beauty FLOAT,
+    clothes FLOAT,
+    technic FLOAT,
+    subscriptions FLOAT,
+    total FLOAT);''')
+connection.commit()
 
 # Database -> operations.
 cursor.execute('''CREATE TABLE IF NOT EXISTS operations(
-    operation_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-    user_id INTEGER,
+    operation_id INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
+    user_id INT,
     category TEXT,
-    value REAL,
-    date TEXT,
-    FOREIGN KEY (user_id) REFERENCES users (user_id))''')
+    value FLOAT,
+    date TEXT);''')
+connection.commit()
--- data/database/__init__.py
+++ data/database/__init__.py
@@ -4,7 +4,7 @@ from data import connection, cursor
 
 def insert_new_user_into_database_users(user_id: int) -> None:
     try:
-        cursor.execute(f'INSERT INTO users VALUES (?, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)',
+        cursor.execute(f'INSERT INTO users VALUES (%s, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)',
                        (user_id,))
     except:
         print(f'> insert user {user_id} into database/users: error.')
@@ -21,12 +21,13 @@ def insert_operation_into_database_operations(user_id: int) -> None:
 
     try:
         # Insert operation data into database/operations.
-        cursor.execute(f'INSERT INTO operations (user_id, category, value, date) VALUES (?, ?, ?, ?)',
+        cursor.execute(f'INSERT INTO operations (user_id, category, value, date) VALUES (%s, %s, %s, %s)',
                        (user_id, category, value, date))
         connection.commit()
+        print('operations good')
 
         # Update user data in database/users.
-        cursor.execute(f'UPDATE users SET {category} = {category} + ?, total = total + ? WHERE user_id = ?',
+        cursor.execute(f'UPDATE users SET {category} = {category} + %s, total = total + %s WHERE user_id = %s',
                        (value, value, user_id))
         connection.commit()
     except:
@@ -37,7 +38,7 @@ def insert_operation_into_database_operations(user_id: int) -> None:
 
 def select_operations_from_database_operations(user_id: int, limit: int = 999999) -> list:
     # Fetch all operations from database/operations.
-    cursor.execute(f'SELECT category, value, date FROM operations WHERE user_id = ? ORDER BY operation_id DESC LIMIT ?',
+    cursor.execute(f'SELECT category, value, date FROM operations WHERE user_id = %s ORDER BY operation_id DESC LIMIT %s',
                    (user_id, limit))
 
     # Last operations: list with tuples like [(operation_data), (operation_data), ...]
@@ -48,7 +49,7 @@ def select_operations_from_database_operations(user_id: int, limit: int = 999999
 
 def delete_last_operation_from_database(user_id: int) -> list:
     # Fetch operation category and value for update database/users.
-    cursor.execute(f'SELECT category, value FROM operations WHERE user_id =? ORDER BY operation_id DESC LIMIT 1',
+    cursor.execute(f'SELECT category, value FROM operations WHERE user_id =%s ORDER BY operation_id DESC LIMIT 1',
                    (user_id,))
     operation_data: tuple = cursor.fetchall()[0]
 
@@ -57,12 +58,12 @@ def delete_last_operation_from_database(user_id: int) -> list:
     value: float = operation_data[1]
 
     # Delete operation value from database/users.
-    cursor.execute(f'UPDATE users SET {category} = {category} - ?, total = total - ? WHERE user_id = ?',
+    cursor.execute(f'UPDATE users SET {category} = {category} - %s, total = total - %s WHERE user_id = %s',
                    (value, value, user_id))
     connection.commit()
 
     # Delete operation from database/operations.
-    cursor.execute(f'DELETE FROM operations WHERE operation_id = (SELECT MAX(operation_id) FROM operations) and user_id = ?',
+    cursor.execute(f'DELETE FROM operations WHERE user_id = %s ORDER BY operation_id DESC LIMIT 1',
                    (user_id,))
     connection.commit()
 
@@ -74,11 +75,11 @@ def delete_last_operation_from_database(user_id: int) -> list:
 
 def delete_all_user_operations_from_database(user_id: int) -> None:
     # Delete all operations from database/operations.
-    cursor.execute(f'DELETE FROM operations WHERE user_id = ?', (user_id, ))
+    cursor.execute(f'DELETE FROM operations WHERE user_id = %s', (user_id, ))
     connection.commit()
 
     # Set default values = 0 in database/users.
     cursor.execute(f'UPDATE users SET products = 0, cafes = 0, auto = 0, transport = 0, home = 0, entertainment = 0, '
                    f'sport = 0, health = 0, education = 0, gifts = 0, beauty = 0, clothes = 0, technic = 0, subscriptions = 0, total = 0 '
-                   f'WHERE user_id = ?', (user_id, ))
+                   f'WHERE user_id = %s', (user_id, ))
     connection.commit()
